381624b6a4da2df8887be4390e590266208e645b
Roman <76100057+RomanMorf@users.noreply.github.com>
2022-02-16 23:19:29 +0200

    ready for merge

--- src/components/SerachBar/index.js
+++ src/components/SerachBar/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.scss'
 
-function SearchBar() {
+function SearchBar({items, onSearch}) {
+  const [ filtered, setFiletred ] = useState('');
+  const [ request, setRequest ] = useState();
+
+  useEffect(() => {
+    onSearch(filtered)
+  }, []);
+
+  function serchHandle(e) {
+    setRequest(e.target.value)
+    const filteredItems = items.filter((item) => item.title.indexOf(request) > -1 )
+    setFiletred(filteredItems)
+    onSearch(filtered)
+  }
+
   return (
     <div className='search_wrapper'>
-      seach bar
+      <input type="text" onChange={(e) => serchHandle(e)} />
     </div>
   )
 }
