6ea7640ca89d12b9b501ca2f095064a1022e3877
Roman <76100057+RomanMorf@users.noreply.github.com>
2022-02-16 18:22:54 +0200

    search bar development - start

---
+++ src/components/SerachBar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss'
+
+function SearchBar() {
+  return (
+    <div className='search_wrapper'>
+      seach bar
+    </div>
+  )
+}
+
+export default SearchBar;
